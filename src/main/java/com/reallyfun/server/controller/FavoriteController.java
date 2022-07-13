51e68ee47bdd23254520a062905f8990856f1a53
Leohh <948750310@qq.com>
2022-07-13 08:12:43 +0800

    fix: 完善收藏API

--- src/main/java/com/reallyfun/server/controller/FavoriteController.java
+++ src/main/java/com/reallyfun/server/controller/FavoriteController.java
@@ -8,38 +8,75 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
+/**
+ * 处理收藏相关请求的控制器类
+ */
 @RestController
-@RequestMapping("favorite")
 public class FavoriteController extends BaseController {
     @Autowired
-    IFavoriteService iFavoriteService;
+    IFavoriteService favoriteService;
 
     /**
      * 收藏游戏
      *
-     * @param gameId  收藏的游戏id
+     * @param gameId  收藏的游戏ID
      * @param session HttpSession对象
      * @return 若收藏成功，返回信息；不成功返回对应错误原因
      */
-    @PostMapping("")
-    public ResponseResult<Void> collect(@RequestParam("game_id") Integer gameId, HttpSession session) {
+    @PostMapping("/favorite")
+    public ResponseResult<Void> collect(
+            @RequestParam("game_id") Integer gameId,
+            HttpSession session
+    ) {
+        // 获取用户对象
         User user = getUserFromSession(session);
-        iFavoriteService.collect(gameId, user.getId());
-        return ResponseResult.getResponseResult("收藏成功");
+
+        // 添加收藏
+        favoriteService.collect(gameId, user.getId());
+
+        // 返回响应结果
+        return ResponseResult.getResponseResult("收藏成功！");
     }
 
     /**
      * 取消收藏游戏
      *
-     * @param gameId  取消收藏的游戏id
+     * @param gameId  取消收藏的游戏ID
      * @param session HttpSession对象
      * @return 若取消成功，返回信息；不成功则赶回对应原因
      */
-    @DeleteMapping("")
-    public ResponseResult<Void> delete(@RequestParam("game_id") Integer gameId, HttpSession session) {
+    @DeleteMapping("/favorite")
+    public ResponseResult<Void> delete(
+            @RequestParam("game_id") Integer gameId,
+            HttpSession session
+    ) {
+        // 获取用户对象
         User user = getUserFromSession(session);
-        iFavoriteService.delete(gameId, user.getId());
+
+        // 删除收藏
+        favoriteService.delete(gameId, user.getId());
+
+        // 返回响应结果
         return ResponseResult.getResponseResult("已取消收藏");
+    }
+
+    /**
+     * 获取某用户的游戏收藏
+     *
+     * @param userId   用户ID
+     * @param pageSize 页面大小
+     * @param pageNum  页码
+     * @return 页面范围内该用户的收藏
+     */
+    @GetMapping("/favorites")
+    public ResponseResult<List<Favorite>> findAllFavoriteByUserIdOfPage(
+            @RequestParam("user_id") Integer userId,
+            @RequestParam("page_size") Integer pageSize,
+            @RequestParam("page_num") Integer pageNum
+    ) {
+        List<Favorite> result = favoriteService.findAllFavoriteByUserIdOfPage(userId, pageSize, pageNum);
+        return ResponseResult.getResponseResult(result);
     }
 }
--- src/main/java/com/reallyfun/server/service/impl/FavoriteServiceImpl.java
+++ src/main/java/com/reallyfun/server/service/impl/FavoriteServiceImpl.java
@@ -2,42 +2,92 @@ package com.reallyfun.server.service.impl;
 
 import com.reallyfun.server.entity.Favorite;
 import com.reallyfun.server.mapper.IFavoriteMapper;
+import com.reallyfun.server.mapper.IGameMapper;
+import com.reallyfun.server.mapper.IUserMapper;
 import com.reallyfun.server.service.IFavoriteService;
 import com.reallyfun.server.service.ex.FavoriteException;
+import com.reallyfun.server.service.ex.TagException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
+import java.util.List;
+
+/**
+ * 处理收藏数据的业务层实现类
+ */
 @Service
 public class FavoriteServiceImpl implements IFavoriteService {
-    @Autowired
-    IFavoriteMapper iFavoriteMapper;
+    @Autowired(required = false)
+    IFavoriteMapper favoriteMapper;
 
+    @Autowired(required = false)
+    IUserMapper userMapper;
+
+    @Autowired(required = false)
+    IGameMapper gameMapper;
+
+    @Override
     public void collect(Integer gameId, Integer userId) {
-        if (iFavoriteMapper.findById(gameId, userId) != null) {
-            throw new FavoriteException("对不起，您已经收藏该游戏");
+        // 判断游戏是否存在
+        if (gameMapper.findById(gameId) == null) {
+            throw new TagException("游戏不存在");
         }
+
+        // 判断是否收藏过该游戏
+        if (favoriteMapper.existByIds(gameId, userId) != null) {
+            throw new FavoriteException("您已经收藏过该游戏");
+        }
+
+        // 构造收藏数据
         Favorite favorite = new Favorite();
         favorite.setGameId(gameId);
         favorite.setUserId(userId);
         favorite.createBy(userId);
-        Integer row = iFavoriteMapper.insertFavorite(favorite);
-        if (row != 1) {
-            throw new FavoriteException("出现错误，请您重试");
+
+        // 添加收藏并判断是否成功
+        Integer result = favoriteMapper.insert(favorite);
+        if (result != 1) {
+            throw new FavoriteException("添加收藏失败");
         }
     }
 
+    @Override
     public void delete(Integer gameId, Integer userId) {
-        if (iFavoriteMapper.findById(gameId, userId) == null) {
-            throw new FavoriteException("对不起，您已经取消收藏该游戏");
+        // 判断游戏是否存在
+        if (gameMapper.findById(gameId) == null) {
+            throw new TagException("游戏不存在");
         }
-        Favorite favorite = new Favorite();
-        favorite.setUserId(userId);
-        favorite.createBy(userId);
-        favorite.setGameId(gameId);
-        Integer row = iFavoriteMapper.deleteFavorite(favorite);
-        if (row != 1) {
-            throw new FavoriteException("出现错误，请稍后重试");
+
+        // 判断是否收藏过该游戏
+        if (favoriteMapper.existByIds(gameId, userId) == null) {
+            throw new FavoriteException("您尚未收藏该游戏");
         }
+
+        // 删除收藏并判断是否成功
+        Integer result = favoriteMapper.deleteFavoriteByIds(gameId, userId);
+        if (result != 1) {
+            throw new FavoriteException("取消收藏失败");
+        }
+    }
+
+    @Override
+    public List<Favorite> findAllFavoriteByUserIdOfPage(Integer userId, Integer pageSize, Integer pageNum) {
+        // 判断用户是否存在
+        if (userMapper.findUserById(userId) == null) {
+            throw new FavoriteException("用户不存在");
+        }
+
+        // 限定查询范围
+        Integer size = Math.min(Math.max(pageSize, 1), 100);
+        Integer num = Math.max(pageNum, 1);
+
+        // 查询并返回结果
+        List<Favorite> result = favoriteMapper.findAllFavoriteByUserIdOfRange(
+                userId,
+                size * (num - 1),
+                size
+        );
+        // TODO: 添加游戏详细数据
+        return result;
     }
 }
