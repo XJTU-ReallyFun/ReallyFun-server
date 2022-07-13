f7af2846a8928ae7e0dd1640a48ae11ac07a48fc
Leohh <948750310@qq.com>
2022-07-13 08:03:28 +0800

    fix: 修改了拦截器代码结构，可控制多个拦截器执行顺序

--- src/main/java/com/reallyfun/server/interceptor/LoginInterceptor.java
+++ src/main/java/com/reallyfun/server/interceptor/LoginInterceptor.java
@@ -1,5 +1,6 @@
 package com.reallyfun.server.interceptor;
 
+import com.reallyfun.server.entity.User;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
@@ -11,27 +12,33 @@ import java.util.List;
 /**
  * 定义处理器拦截器
  */
-public class ExampleInterceptor implements HandlerInterceptor {
-    // 该⽅法将在请求处理之前被调⽤
+public class LoginInterceptor implements HandlerInterceptor {
+    // 该方法将在请求处理之前被调用
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
                              Object handler) throws Exception {
-        System.out.println(">>>ExampleInterceptor.preHandle");
-        if (request.getRequestURL().indexOf("example") != -1) {
-            // 重定向到指定的页面
-            response.sendRedirect("/path/to/redirect");
-            // 当返回false时，表示请求结束，后续的Interceptor和Controller都不会再执行
+        System.out.println(">>>LoginInterceptor.preHandle");
+        // 获取当前用户对象
+        User user = (User) request.getSession().getAttribute("user");
+
+        // 若用户未登录，则返回错误信息并结束
+        if (user == null) {
+            response.getWriter().write("{\"code\":3000,\"message\":\"You have not logged in.\"}");
+            response.setContentType("application/json");
+            response.setStatus(200);
             return false;
         }
-        // 当返回值true时，就会继续调用下⼀个Interceptor的preHandle方法，如果已经是最后⼀个
-        // Interceptor的时候，就会调用当前请求的Controller方法
+
+        // 否则继续进行
         return true;
     }
 
     public void register(InterceptorRegistry registry) {
         // 白名单
         List<String> patterns = new ArrayList<>();
-        patterns.add("/example/*");
+        patterns.add("/user/register");
+        patterns.add("/user/login");
+        patterns.add("/games");
 
         // 通过注册工具添加拦截器
         registry.addInterceptor(this)
